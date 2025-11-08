2d292d62e4368e133fd08fd0c47dfaeafd8c59bf
Gonçalo_Rodrigues <G.dRCPRO@protonmail.com>
2025-11-08 10:24:34 +0000

    Start

---
+++ src/AnimatedOverlay.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class AnimatedOverlay {
+    
+    public AnimatedOverlay(List<String> caminhosFrames, int x, int y,
+                           int velocidadeMs, double fatorEscala){
+
+    }
+
+    private void carregarFrames(List<String> caminhosFrames, double fatorEscala){
+
+    }
+
+    private void configurarJanela(int x, int y){
+
+    }
+
+    private void iniciarAnimacao(int velocidadeMs){
+
+    }
+
+    private void proximoFrame(){
+
+    }
+
+
+}
